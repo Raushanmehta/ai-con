6dbe8637ef811cb45777e0f59bc721a0583ce60b
Raushanmehta <raushanmehta2184@gmail.com>
2026-02-07 23:08:31 +0530

    fixed responsive

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -5,33 +5,38 @@ import FormSection from '../_components/FormSection'
 import OutputSection from '../_components/OutputSection'
 import Templates from '@/app/(data)/Template'
 
-
 interface PROPS {
   params: Promise<{
     'template-slug': string
   }>
 }
-export default async function Page  ({ params }: PROPS) {
 
+export default async function Page({ params }: PROPS) {
   const resolvedParams = await params
 
-  const filterTemplate = Templates?.filter(
+  const selectedTemplate = Templates.find(
     (item) => item.slug === resolvedParams['template-slug']
   )
 
-  
   return (
-    <div className="p-5">
+    <div className="p-4 sm:p-6 max-w-7xl mx-auto">
+      {/* Back button */}
       <Link href="/dashboard">
-        <Button className="border border-gray-800 bg-black hover:bg-pink-500">
-          <ArrowLeft /> Back
+        <Button className="border border-gray-800 bg-black hover:bg-pink-500 mb-4">
+          <ArrowLeft className="mr-2" />
+          Back
         </Button>
       </Link>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mt-5">
-        <FormSection  selectedTemplate={filterTemplate[0]} />
+      {/* Responsive layout */}
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-10">
+        {/* Form */}
+        <div className="lg:col-span-1">
+          <FormSection selectedTemplate={selectedTemplate} />
+        </div>
 
-        <div className="col-span-2">
+        {/* Output */}
+        <div className="lg:col-span-2">
           <OutputSection />
         </div>
       </div>
